425a17dc4dd59ff240bf1a7e61868f9fe029941e
Roy Joseph Argumido <57471158+rjargumido@users.noreply.github.com>
2020-03-22 20:50:46 +0800

    Update Hydra_Serial_Nano.ino

--- Sample Arduino Codes/Hydra_Serial_Nano/Hydra_Serial_Nano.ino
+++ Sample Arduino Codes/Hydra_Serial_Nano/Hydra_Serial_Nano.ino
@@ -1,31 +1,30 @@
 #include <ArduinoJson.h>
 
-const size_t capacity = JSON_OBJECT_SIZE(3) + 100;
+const size_t capacity = JSON_OBJECT_SIZE(2) + 50;
 DynamicJsonDocument recvData(capacity);
 
-void setup() {
-  Serial.begin(9600);
-}
-
-void loop() {
+void recieveData() {
   DeserializationError error = deserializeJson(recvData, Serial);
   if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
+    //Serial.print(F("deserializeJson() failed: "));
+    //Serial.println(error.c_str());
     return;
   }else {
     const char* origin = recvData["origin"];
     const int request = recvData["request"];
-    const int value = recvData["value"];
   
     Serial.print("Origin: ");
     Serial.println(origin);
   
     Serial.print("Request: ");
     Serial.println(request);
-  
-    Serial.print("Value: ");
-    Serial.println(value);
   }
-  delay(1000);
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  recieveData();
 }
