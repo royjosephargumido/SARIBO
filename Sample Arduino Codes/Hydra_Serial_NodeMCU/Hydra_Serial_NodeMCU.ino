5c21d4ff6c861ef041238e3852b80b88cd1cab45
Roy Joseph Argumido <57471158+rjargumido@users.noreply.github.com>
2020-03-22 20:50:59 +0800

    Update Hydra_Serial_NodeMCU.ino

--- Sample Arduino Codes/Hydra_Serial_NodeMCU/Hydra_Serial_NodeMCU.ino
+++ Sample Arduino Codes/Hydra_Serial_NodeMCU/Hydra_Serial_NodeMCU.ino
@@ -3,19 +3,17 @@
 
 SoftwareSerial s(3,1);
 
-const size_t capacity = JSON_OBJECT_SIZE(3) + 100;
+const size_t capacity = JSON_OBJECT_SIZE(2) + 50;
 DynamicJsonDocument transData(capacity);
 
 void setup() {
   s.begin(9600);
 }
 
-
 void loop() {
   transData["origin"] = "92EC9416";
   transData["request"] = 11;
-  transData["value"] = 765;
 
   serializeJson(transData, s);
-  delay(1000);
+  delay(250);
 }
