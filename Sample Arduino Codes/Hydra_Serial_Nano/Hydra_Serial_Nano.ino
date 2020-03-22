1cb662cc7593bf7ea61a536150b2c982ce2209e5
Roy Joseph Argumido <57471158+rjargumido@users.noreply.github.com>
2020-03-22 19:46:46 +0800

    Update Hydra_Serial_Nano.ino

--- Sample Arduino Codes/Hydra_Serial_Nano/Hydra_Serial_Nano.ino
+++ Sample Arduino Codes/Hydra_Serial_Nano/Hydra_Serial_Nano.ino
@@ -1,24 +1,31 @@
-#include <SoftwareSerial.h> //Included SoftwareSerial Library
 #include <ArduinoJson.h>
 
-//Started SoftwareSerial at RX and TX pin of ESP8266/NodeMCU
-SoftwareSerial s(3,1);
+const size_t capacity = JSON_OBJECT_SIZE(3) + 100;
+DynamicJsonDocument recvData(capacity);
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  const size_t capacity = JSON_OBJECT_SIZE(6);
-  DynamicJsonDocument doc(capacity);
+  DeserializationError error = deserializeJson(recvData, Serial);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }else {
+    const char* origin = recvData["origin"];
+    const int request = recvData["request"];
+    const int value = recvData["value"];
   
-  doc["id"] = "leaf01-101-11x57x34-2x21x2020";
-  doc["datesent"] = "2/21/2020";
-  doc["timesent"] = "11:57:34";
-  doc["origin"] = "689DB004-E03A-4A37-9944-02CB7B2844BE";
-  doc["request"] = 101;
-  doc["value"] = 765;
+    Serial.print("Origin: ");
+    Serial.println(origin);
   
-  serializeJson(doc, Serial);
-  delay(500);
+    Serial.print("Request: ");
+    Serial.println(request);
+  
+    Serial.print("Value: ");
+    Serial.println(value);
+  }
+  delay(1000);
 }
