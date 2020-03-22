9e98466f90cee660b8744cd6ac763d206a1ed01b
Roy Joseph Argumido <57471158+rjargumido@users.noreply.github.com>
2020-03-22 19:46:28 +0800

    Update Hydra_Serial_NodeMCU.ino

--- Sample Arduino Codes/Hydra_Serial_NodeMCU/Hydra_Serial_NodeMCU.ino
+++ Sample Arduino Codes/Hydra_Serial_NodeMCU/Hydra_Serial_NodeMCU.ino
@@ -1,39 +1,21 @@
-#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
-int data; //Initialized variable to store recieved data
+SoftwareSerial s(3,1);
+
+const size_t capacity = JSON_OBJECT_SIZE(3) + 100;
+DynamicJsonDocument transData(capacity);
 
 void setup() {
-  //Serial Begin at 9600 Baud 
-  Serial.begin(9600);
+  s.begin(9600);
 }
 
+
 void loop() {
-  const size_t capacity = JSON_OBJECT_SIZE(3) + 40;
-  DynamicJsonDocument doc(capacity);
+  transData["origin"] = "92EC9416";
+  transData["request"] = 11;
+  transData["value"] = 765;
 
-  // Parse JSON object
-  DeserializationError error = deserializeJson(doc, Serial);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-    return;
-  }
-
-  // Extract values
-  const char* id = doc["id"];             // "leaf01-101-11x57x34-2x21x2020"
-  const char* datesent = doc["datesent"]; // "2/21/2020"
-  const char* timesent = doc["timesent"]; // "11:57:34"
-  const char* origin = doc["origin"];     // "689DB004-E03A-4A37-9944-02CB7B2844BE"
-  int request = doc["request"];           // 101
-  int value = doc["value"];               // 765
-
-  Serial.println(id);
-  Serial.println(datesent);
-  Serial.println(timesent);
-  Serial.println(origin);
-  Serial.println(request);
-  Serial.println(value);
-  
-  delay(500);
+  serializeJson(transData, s);
+  delay(1000);
 }
