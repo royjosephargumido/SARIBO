5b3ead3be93e2fccfda1794f96926e39be728bc3
Roy Joseph Argumido <57471158+rjargumido@users.noreply.github.com>
2020-04-16 02:01:24 +0800

    Leaf Module v1.3.1

--- Source/Leaf/Leaf.ino
+++ Source/Leaf/Leaf.ino
@@ -1,6 +1,6 @@
 /*
   The S.A.R.I.B.O. Leaf Module - NodeMCU-12E v3 esp8266 Module Code
-  Version 1.3.0 Revision April 14, 2020
+  Version 1.3.1 Revision April 16, 2020
   Systematic and Automated Regulation of Irrigation systems for Backyard farming Operations
   
   BSD 3-Clause License
@@ -35,7 +35,6 @@
 
 #include <SD.h>
 #include <SPI.h>
-#include <Wire.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 
@@ -46,42 +45,38 @@ const int smAP = 0;
 //====================================================
 
 //======================= CACHE ======================
+const int _sleep = 5;
+const int maxBuffer = 2000;
+const long int sleepTime = 1000000 * _sleep;
+const char* coreFN = "System/CoreConfig.txt";
+
 File file;
 IPAddress myIP;
+int ngid = 0;
 int sdfail = 0;
-int serverfail = 0;
 int hostfail = 0;
+int loadfail = 0;
 int sconnfail = 0;
+int serverfail = 0;
+int irrReadfail = 1;
 bool startWater = false;
-
-String device;
-String dmsheader;
-String dmsversion;
-String dcreated;
-String tcreated;
-String owner;
-String dmodified;
-String tmodified;
-String modifiedby;
-String umode;
+int reads = 0;
+unsigned long bootInit;
 
 String repDir;
 String logsDir;
-
 String root;
 String local;
-
 String ssid;
 String key;
 String host;
 String reqpath;
 int port;
-
+int rFail = 1;
 int rSoil;
 int rGen;
 int rOpen;
 int rClose;
-
 String wut;
 int smrp;
 int maxd;
@@ -90,8 +85,6 @@ int ideal;
 //====================================================
 
 //============== ARDUINOJSON COMPONENTS ==============
-const int maxBuffer = 2000;
-
 const size_t desBuffer = JSON_OBJECT_SIZE(4) + maxBuffer;
 const size_t fileBuffer = JSON_OBJECT_SIZE(28) + maxBuffer;
 DynamicJsonDocument settingsData(fileBuffer);
@@ -100,12 +93,19 @@ DynamicJsonDocument exchangeData(desBuffer);
 
 void initSD() {
   pinMode(sdCS, OUTPUT);
-
   while(!SD.begin(sdCS)) {
-    Serial.println("Unable to detect the SD Card module!");
-    sdfail++;
-    delay(1000);
+    if(sdfail == 60) {
+      ESP.restart();
+    } else {
+      Serial.println("Unable to detect the SD Card module!");
+      sdfail++;
+      delay(1000);
+    }
   }
+}
+
+String generateTID() {
+  return local + '-' + String(++ngid);
 }
 
 String decodeJsonData(const DeserializationError error) {
@@ -118,14 +118,14 @@ String decodeJsonData(const DeserializationError error) {
 void loadLeafSettings() {
   char data[maxBuffer];
   int i = 0;
-
-  do {
-    file = SD.open("System/CoreConfig.txt");
-    if(!file) {
-      Serial.println("Unable to load settings!");
-      delay(500);
-    }
-  }while(!file);
+  
+  file = SD.open(coreFN);
+  while(!file) {
+    loadfail++;
+    Serial.println("Unable to load settings!");
+    delay(1000);
+    file = SD.open(coreFN);
+  }
 
   while(file.available()) {
     data[i] = file.read();
@@ -133,36 +133,21 @@ void loadLeafSettings() {
   }
   data[i] = '\0';
 
-  decodeJsonData(deserializeJson(settingsData, data));
-    
-  device = settingsData["device"].as<String>();
-  dmsheader = settingsData["header"].as<String>();
-  dmsversion = settingsData["dmsver"].as<String>();
-  dcreated = settingsData["dcreated"].as<String>();
-  tcreated = settingsData["tcreated"].as<String>();
-  owner = settingsData["owner"].as<String>();
-  dmodified = settingsData["dmodified"].as<String>();
-  tmodified = settingsData["tmodified"].as<String>();
-  modifiedby = settingsData["modifiedby"].as<String>();
-  umode = settingsData["umode"].as<String>();
+  String decodeSF = decodeJsonData(deserializeJson(settingsData, data));
   
   repDir = settingsData["repdir"].as<String>();
   logsDir = settingsData["logsdir"].as<String>();
-
   rSoil = settingsData["rsoil"].as<int>();
   rGen = settingsData["rgen"].as<int>();
   rOpen = settingsData["ropen"].as<int>();
   rClose = settingsData["rclose"].as<int>();
-
   root = settingsData["root"].as<String>();
   local = settingsData["local"].as<String>();
-  
   ssid = settingsData["ssid"].as<String>();
   key = settingsData["key"].as<String>();
   host = settingsData["host"].as<String>();
   reqpath = settingsData["reqpath"].as<String>();
   port = settingsData["port"].as<int>();
-  
   wut = settingsData["wut"].as<String>();
   smrp = settingsData["smrp"].as<int>();
   maxd = settingsData["maxd"].as<int>();
@@ -176,67 +161,41 @@ void loadLeafSettings() {
 void connectToServer() {  
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, key);
-
-  Serial.println();
   
+  Serial.println("Connecting to " + ssid + "...");
   while(WiFi.status() != WL_CONNECTED) {
-    Serial.print("Connecting to ");
-    Serial.print(ssid);
-    Serial.println("...");
+    
     serverfail++;
     delay(1000);
   }
-  
-  Serial.println();
-  Serial.print("Connected to Server after ");
-  Serial.print(serverfail);
-  Serial.println(" seconds.");
 
   myIP = WiFi.localIP();
-  Serial.print("Leaf IP Address: ");
-  Serial.println(myIP);
+  Serial.println("Connected to Server after " + (String)serverfail + " seconds.");
+  Serial.println("Leaf IP Address: " + myIP.toString());
 }
 
 void sendrequest(const int rCode, const int value) {
   WiFiClient client;
   
   while(!client.connect(host, port)) {
-    Serial.print("Unable to reach host(");
-    Serial.print(host);
-    Serial.print(":");
-    Serial.print(port);
-    Serial.println("). Reconnecting...");
+    //Serial.println("Unable to reach host(" + host + ":" + String(port) + "). Reconnecting...");
     hostfail++;
     delay(1000);
   }
 
-  Serial.print("Connected to host(");
-  Serial.print(host);
-  Serial.print(":");
-  Serial.print(port);
-  Serial.print(") after ");
-  Serial.print(hostfail);
-  Serial.println(" seconds.");
-
-  exchangeData["id"] = "**sample_id**";
+  String reqID = generateTID();
+  exchangeData["id"] = reqID;
   exchangeData["origin"] = local;
   exchangeData["request"] = rCode;
   exchangeData["value"] = value;
-
-  Serial.println();
-  Serial.println("****************************************");
-  Serial.println("Request sent!");
-
+  
   String payload = "";
   serializeJson(exchangeData, payload);
-  client.print(String("GET ") + reqpath + payload + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+  client.print("GET " + reqpath + payload + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
 
-  Serial.println("Waiting for server response...");
-  Serial.println("****************************************");
-  
   unsigned long timeout = millis();
   while(client.available() == 0) {
-    if(millis() - timeout > 5000) {
+    if(millis() - timeout > 60000) {
       Serial.println("Server unreachable. Reconnecting...");
       sconnfail++;
       sendrequest(rCode, value);
@@ -244,48 +203,44 @@ void sendrequest(const int rCode, const int value) {
     }
   }
 
-  Serial.print("Connected to server after ");
-  Serial.print(sconnfail);
-  Serial.println(" seconds.");
+  Serial.println("\n****************************************");
+  Serial.println("Connected to host(" + host + ":" + port + ") after " + String(hostfail) + " seconds.");
+  Serial.println("Connected to server after " + String(sconnfail) + " seconds.");
+  Serial.println("Request sent!");
+  Serial.println("****************************************");
 
   String responsePayload = "";
   while(client.available()) {
     responsePayload = client.readStringUntil('\r');
   }
+  Serial.println();
+  Serial.println(decodeJsonData(deserializeJson(exchangeData, responsePayload)));
 
-  decodeJsonData(deserializeJson(exchangeData, responsePayload));
-
-  String id = exchangeData["id"].as<String>();
+  String resID = exchangeData["id"].as<String>();
   const char* origin = exchangeData["origin"];
   int request = exchangeData["request"];
   const char* _value = exchangeData["value"];
 
-  Serial.println();
+  if(!(reqID == resID)) {
+    Serial.println("Empty response! Resending request.");
+    sendrequest(rCode, value);
+  } else {
+    Serial.println("\n=============== REQUEST ===============");
+    Serial.println("ID: " + String(reqID));
+    Serial.println("Origin: " + local);
+    Serial.println("Request: " + String(rCode));
+    Serial.println("Value: " + String(value));
+    Serial.println("======================================");
   
-  Serial.println("=============== REQUEST ===============");
-  Serial.print("ID: ");
-  Serial.println("**sample_id**");
-  Serial.print("Origin: ");
-  Serial.println(local);
-  Serial.print("Request: ");
-  Serial.println(rCode);
-  Serial.print("Value: ");
-  Serial.println(value);
-  Serial.println("======================================");
-
-  Serial.println("============== RESPONSE ==============");
-  Serial.print("ID: ");
-  Serial.println(id);
-  Serial.print("Origin: ");
-  Serial.println(origin);
-  Serial.print("Request: ");
-  Serial.println(request);
-  Serial.print("Value: ");
-  Serial.println(_value);
-  Serial.println("======================================");
-  
-  client.stop();
-  ESP.deepSleep(10e6);
+    Serial.println("============== RESPONSE ==============");
+    Serial.println("ID: " + String(resID));
+    Serial.println("Origin: " + String(origin));
+    Serial.println("Request: " + String(request));
+    Serial.println("Value: " + String(_value));
+    Serial.println("======================================");
+    
+    client.stop();
+  }
 }
 
 void perfSMRead() {
@@ -295,68 +250,89 @@ void perfSMRead() {
   digitalWrite(smVCC, HIGH);
   delay(3000);
 
+  reads++;
+  Serial.println("\nSoil moisture reading #" + String(reads));
+  Serial.println("***************************************");
   while(count < smrp) {
     int smv = analogRead(smAP);
     readings += smv;
     count++;
+    Serial.println("Reading #" + String(count) + ": " + String(smv));
     delay(1000);
-
-    Serial.print("Reading #");
-    Serial.print(count);
-    Serial.print(": ");
-    Serial.println(smv);
   }
   
   const int finalSMV = readings / count;
-
-  Serial.println();
-  Serial.print("Final Soil Moisture value: ");
-  Serial.println(finalSMV);
+  Serial.println("***************************************");
+  Serial.println("Final soil moisture value: " + String(finalSMV));
 
   if(finalSMV >= maxd) {
-    Serial.println("Irregular reading! Repeating soil moisture reading.");
-    sendrequest(rSoil, finalSMV);
+    Serial.println("Irregular reading! Fail #" + String(irrReadfail));
+    if((irrReadfail % 5) == 0) {
+      Serial.println("Soil moisture reading sent to server.");
+      Serial.println("***************************************");
+      sendrequest(rSoil, finalSMV);
+    }
+    
+    Serial.println("\n***************************************");
+    Serial.println("Repeating soil moisture reading.");
+    Serial.println("***************************************");
+    irrReadfail++;
     perfSMRead();
   }
   else if (finalSMV < maxd && finalSMV > mind)  {
-    Serial.println("Soil is dry! Starting watering process.");
+    irrReadfail = 0;
+    Serial.println("Soil is dry!\nStarting watering process.");
+    Serial.println("***************************************");
     sendrequest(rOpen, finalSMV);
     startWater = true;
+    Serial.println("\nWaiting for the soil absorption interval (5 minutes)...");
+    delay(5000);
+    sendrequest(rClose, finalSMV);
+    perfSMRead();
   }
   else if (finalSMV <= mind && finalSMV > ideal) {
-    Serial.println("Soil is in ideal soil moisture value.");    
+    irrReadfail = 0;
+    Serial.println("Soil is in ideal soil moisture value.");
     if(startWater == true) {  //Watering Process Started
       Serial.println("Ending watering process.");
+      Serial.println("***************************************");
       sendrequest(rClose, finalSMV);
     }
     else {
       Serial.println("Logging soil moisture value.");
+      Serial.println("***************************************");
       sendrequest(rGen, finalSMV);
     }
   }
   else {
+    irrReadfail = 0;
     Serial.println("Soil is wet!");
+    Serial.println("***************************************");
     sendrequest(rGen, finalSMV);
   }
-  Serial.println("***************************************");
+  digitalWrite(smVCC, LOW);
 }
 
 void setup() {
+  bootInit = millis();
+  pinMode(smVCC, OUTPUT);
+  digitalWrite(smVCC, LOW);
+  
   Serial.begin(115200);
   delay(3000);
 
-  Serial.println();
-
-  pinMode(smVCC, OUTPUT);
-  digitalWrite(smVCC, LOW);
-
+  Serial.println("\n");
   initSD();
   loadLeafSettings();
   connectToServer();
-  
-  Serial.println();
-  
   perfSMRead();
+
+  int runSecs = (millis() - bootInit) / 1000;
+  int runMins = runSecs / 60;
+  Serial.println("\nRuntime: " + String(runMins) + " minutes and " + String(runSecs % 60) + " seconds");
+  
+  Serial.flush();
+  ESP.deepSleep(sleepTime);
 }
 
 void loop() {
